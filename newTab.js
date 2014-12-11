95d4d211c9a1694d43c47fd2874723a33cb8e2eb
distrill <brent@arthurkn.net>
2014-12-10 16:50:51 -0800

    added some functionality to the the search (can now search youtube, images, and wiki directly)

--- newTab.js
+++ newTab.js
@@ -1,19 +1,69 @@
 function openOrSearch()
 {
     var x = document.getElementById("where_to");
-    // var text = "http://" + x.value;
     var text = x.value;
-    if( text[ text.length - 3 ] === '.' || text[ text.length - 4 ] === '.' )
+    var words = text.split( " " );
+    var destination = "";
+    switch( words[ 0 ])
     {
-        var destination = "http://" + text;
-        window.open( destination, "_self" );
+        case 'l':
+            destination = "http://" + words[ 1 ];
+            window.open( destination, "_self" );
+            break;
+        case 's':
+            destination = "https://www.google.ca/#safe=off&q=";
+            for( var i = 1; i < words.length; i++ )
+            {
+                destination += words[ i ];
+                destination += "+";
+            }
+            window.open( destination, "_self" );
+            break;
+        case 'youtube':
+            destination = 'https://www.youtube.com/results?search_query=';
+            for( var i = 1; i < words.length; i++ )
+            {
+                destination += words[ i ];
+                destination += "+";
+            }
+            window.open( destination, "_self" );
+            break;
+        case 'image':
+            destination = 'https://www.google.ca/search?site=&tbm=isch&source=hp&biw=1568&bih=769&q=';
+            for( var i = 1; i < words.length; i++ )
+            {
+                destination += words[ i ];
+                destination += "+";
+            }
+            window.open( destination, "_self" );
+            break;
+         case 'wiki':
+            destination = 'http://en.wikipedia.org/wiki/';
+            for( var i = 1; i < words.length; i++ )
+            {
+                destination += words[ i ];
+                destination += "_";
+            }
+            window.open( destination, "_self" );
+            break;
+        default:
+            if( text[ text.length - 3 ] === '.' || text[ text.length - 4 ] === '.' )
+            {
+                destination = "http://" + text;
+                window.open( destination, "_self" );
+            }
+            else
+            {
+                destination = "https://www.google.ca/#safe=off&q=" + text;
+                window.open( destination, "_self" );
+            }
+            break;
     }
-    else
-    {
-        var destination = "https://www.google.ca/#safe=off&q=" + text;
-        window.open( destination, "_self" );
-    }
+    
 }
+
+
+
 
 function openAll()
 {
